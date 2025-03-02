256bb5a591c872930b50c004f27f6929b3834017
lsh9672 <lsh80165@gmail.com>
2025-03-02 13:01:38 +0900

    Feat : 알고리즘 6주차 2문제(디렉토리 위치 변경)

--- src/baekjoon/week6/BOJ2342_DanceDanceRevolution.java
+++ src/baekjoon/week6/BOJ2342_DanceDanceRevolution.java
@@ -1,4 +1,4 @@
-package week6;
+package baekjoon.week6;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
--- src/baekjoon/week6/BOJ27653_최소트리분할.java
+++ src/baekjoon/week6/BOJ27653_최소트리분할.java
@@ -1,12 +1,9 @@
-package week6;
+package baekjoon.week6;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
